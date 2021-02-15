dd1c192f2b8b307111ded47edeef66d84c992c8f
mccleskeyc <mccleskeyc@gmail.com>
2021-02-15 17:44:24 -0500

    commit tree 8d9f5ebc438abd70166ed56e6f5a8cfa5fdf2cb2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    let thankYouCards = []
+    for (let i = 0; i < name.length; i++) {
+        thankYouCards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(firstNumber) {
+    while (firstNumber > 0) {
+        console.log(firstNumber);
+        firstNumber -=1;
+    }
+    console.log(firstNumber);
+}
